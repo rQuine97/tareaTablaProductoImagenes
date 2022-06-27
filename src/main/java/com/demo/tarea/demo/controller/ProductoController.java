23d919d936c44d47632fdd836334da38531cf808
rQuine97 <rquine1697@gmail.com>
2022-06-27 00:03:14 -0500

    version2-edit images

--- src/main/java/com/demo/tarea/demo/controller/ProductoController.java
+++ src/main/java/com/demo/tarea/demo/controller/ProductoController.java
@@ -23,60 +23,60 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class ProductoController {
-    
+
     @Autowired
     private ProductoService service;
-    
+
     @GetMapping("/")
     public String index() {
         return "index";
     }
-    
+
     @GetMapping("/productos")
     public String getProductos(Model modelo) {
         modelo.addAttribute("productos", service.readAll());
         return "productos";
     }
-    
+
     @GetMapping("/productos/nuevo")
-    public String showFormRegisterProducto(Model modelo){
+    public String showFormRegisterProducto(Model modelo) {
         Producto producto = new Producto();
         modelo.addAttribute("producto", producto);
         return "crear_producto";
     }
-    
+
     @PostMapping("/productos")
-    public String addProducto(@ModelAttribute("producto") Producto producto, @RequestParam("file") MultipartFile imagen){
-        
-        if(!imagen.isEmpty()){
+    public String addProducto(@ModelAttribute("producto") Producto producto, @RequestParam("file") MultipartFile imagen) {
+
+        if (!imagen.isEmpty()) {
             String ruta = "D://Imagenes-Netbeans//files1";
-            
+
             try {
                 byte[] bytesImg = imagen.getBytes();
-                Path rutacompleta = Paths.get(ruta+"//"+imagen.getOriginalFilename());
-                Files.write(rutacompleta,bytesImg);
+                Path rutacompleta = Paths.get(ruta + "//" + imagen.getOriginalFilename());
+                Files.write(rutacompleta, bytesImg);
                 producto.setImagen(imagen.getOriginalFilename());
             } catch (IOException e) {
-                System.out.println("Error: "+e);
+                System.out.println("Error: " + e);
             }
         }
         service.create(producto);
         return "redirect:/productos";
     }
-    
+
     @GetMapping("/producto/detalle/{idproducto}")
-    public String detalleProducto(@PathVariable("idproducto") int idproducto, Model model){
+    public String detalleProducto(@PathVariable("idproducto") int idproducto, Model model) {
         Producto producto = service.read(idproducto);
         model.addAttribute("producto", producto);
         return "detalle_producto";
     }
-    
+
     @GetMapping("/producto/editar/{idproducto}")
-    public String showFormProducto(@PathVariable Integer idproducto, Model model){
+    public String showFormProducto(@PathVariable Integer idproducto, Model model) {
         model.addAttribute("producto", service.read(idproducto));
         return "editar_producto";
     }
-    
+
 //    @PostMapping("/productos")
 //    public String addProducto(@ModelAttribute("producto") Producto producto, @RequestParam("file") MultipartFile imagen){
 //        
@@ -95,26 +95,38 @@ public class ProductoController {
 //        service.create(producto);
 //        return "redirect:/productos";
 //    }
-    
     @PostMapping("/producto/{idproducto}")
-    public String editProducto(@PathVariable Integer idproducto, @ModelAttribute("producto") Producto producto, Model model){
-        Producto productoExistente = service.read(idproducto);
-        productoExistente.setIdproducto(idproducto);
-        productoExistente.setNombre(producto.getNombre());
-        productoExistente.setPrecio(producto.getPrecio());
-        productoExistente.setCantidad(producto.getCantidad());
-      
-        //productoExistente.setImagen(producto.getImagen());
+    public String editProducto(@PathVariable Integer idproducto, @ModelAttribute("producto") Producto producto, @RequestParam("file") MultipartFile imagen) {
         
-        service.update(productoExistente);
-  
+        if (!imagen.isEmpty()) {
+            String ruta = "D://Imagenes-Netbeans//files1";
+
+            try {
+                byte[] bytesImg = imagen.getBytes();
+                Path rutacompleta = Paths.get(ruta + "//" + imagen.getOriginalFilename());
+                Files.write(rutacompleta, bytesImg);
+                Producto productoExistente = service.read(idproducto);
+               
+                productoExistente.setIdproducto(idproducto);
+                productoExistente.setNombre(producto.getNombre());
+                productoExistente.setPrecio(producto.getPrecio());
+                productoExistente.setCantidad(producto.getCantidad());
+                productoExistente.setImagen(imagen.getOriginalFilename());
+                
+                service.update(productoExistente);
+                
+            } catch (IOException e) {
+                System.out.println("Error: " + e);
+            }
+        }
+
         return "redirect:/productos";
     }
-    
+
     @GetMapping("/producto/{idproducto}")
-    public String deleteProducto(@PathVariable Integer idproducto){
+    public String deleteProducto(@PathVariable Integer idproducto) {
         service.delete(idproducto);
         return "redirect:/productos";
     }
-    
+
 }
